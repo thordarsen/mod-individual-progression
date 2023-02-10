f294e832bf0562086ca69ed335504e15f9260170
thordarsen <50306469+thordarsen@users.noreply.github.com>
2023-02-10 14:10:56 -0500

    Update IndividualProgression_loader.cpp

--- src/IndividualProgression_loader.cpp
+++ src/IndividualProgression_loader.cpp
@@ -3,16 +3,9 @@
  */
 
 // From SC
-void AddSC_mod_individual_progression();
 void AddSC_boss_onyxia_40();
 void AddSC_instance_onyxias_lair_40();
-void AddSC_instance_blackrock_spire_50_59_B();
-void AddSC_instance_molten_core_60_1_A();
-void AddSC_quest_jail_break_60_2();
-void AddSC_quest_the_masquerade_60_2();
 void AddSC_boss_lord_kazzak_60_2();
-void AddSC_boss_chromaggus_60_2_A();
-void AddSC_boss_drakkisath_50_59_B();
 void AddSC_instance_naxxramas_combined();
 void AddSC_boss_anubrekhan_40();
 void AddSC_boss_faerlina_40();
@@ -30,28 +23,14 @@ void AddSC_boss_razuvious_40();
 void AddSC_boss_sapphiron_40();
 void AddSC_boss_thaddius_40();
 void AddSC_npc_omarion();
-void AddSC_Ipp_Wotlk_Modded_Scripts();
 void AddSC_aq_scripts();
-void AddSC_cot_scripts();
-void AddSC_ipp_spell_scripts();
-void AddSC_individualProgression_commandscript();
-void AddSC_mod_individual_progression_awareness();
-void AddSC_mod_individual_progression_player();
-void AddSC_npc_archmage_timear();
 
 
 void Addmod_individual_progressionScripts()
 {
-    AddSC_mod_individual_progression();
     AddSC_boss_onyxia_40();
     AddSC_instance_onyxias_lair_40();
-    AddSC_instance_blackrock_spire_50_59_B();
-    AddSC_instance_molten_core_60_1_A();
-    AddSC_quest_jail_break_60_2();
-    AddSC_quest_the_masquerade_60_2();
     AddSC_boss_lord_kazzak_60_2();
-    AddSC_boss_chromaggus_60_2_A();
-    AddSC_boss_drakkisath_50_59_B();
     AddSC_instance_naxxramas_combined();
     AddSC_boss_anubrekhan_40();
     AddSC_boss_faerlina_40();
@@ -69,12 +48,5 @@ void Addmod_individual_progressionScripts()
     AddSC_boss_sapphiron_40();
     AddSC_boss_thaddius_40();
     AddSC_npc_omarion();
-    AddSC_Ipp_Wotlk_Modded_Scripts();
     AddSC_aq_scripts();
-    AddSC_cot_scripts();
-    AddSC_ipp_spell_scripts();
-    AddSC_individualProgression_commandscript();
-    AddSC_mod_individual_progression_awareness();
-    AddSC_mod_individual_progression_player();
-    AddSC_npc_archmage_timear();
 }
